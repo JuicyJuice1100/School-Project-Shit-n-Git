ca455b4463828faa1e7bf63f59fd09dece81fe77
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2017-09-19 23:22:00 -0500

    finished lab 2 cs 271

--- Fall 2017/CS 271/Lab2/src/lab2/Sort.java
+++ Fall 2017/CS 271/Lab2/src/lab2/Sort.java
@@ -1,12 +1,24 @@
 /*
-n          SelectionSort(ms)    ArraySort (ms)       
+n          SelectionSort (ms)   ArraySort (ms)    
 10         0                    0                    
 100        0                    0                    
 1000       3                    1                    
 10000      79                   1                    
 100000     3906                 7                    
 1000000    405259               16                   
-10000000   46910461             121   
+10000000   46910461             121  
+
+Selection sort big O: O[n^2]
+Array.sort big O: O[log(log(n))]
+
+N                       Selection (s)                       ArraySort (s)
+100,000,000             N^2 * .0079 * 1000(it's huge)       210
+10,000,000,000          N^2 * .0079 * 1000                  360
+1,000,000,000,000       N^2 * .0079 * 1000                  500
+1,000,000,000,000,000   "" ""                               800
+
+calculator can't fit that big of numbers sooo I just estimated without it after
+10,000,000,000.  ArraySort I used log(log(n)) * .0079 * 1000 to convert to seconds.
  */
 package lab2;
 
