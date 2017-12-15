d85c4b9527cb51f0dc576be417394893e8807252
JuicyJuice1100 <espiritu.l.justin@gmail.com>
2017-12-14 19:21:11 -0600

    "finished" lab 12 cs 271

--- Fall 2017/CS 271/Lab12/Lab12/src/lab12/Lab12.java
+++ Fall 2017/CS 271/Lab12/Lab12/src/lab12/Lab12.java
@@ -5,17 +5,33 @@
  */
 package lab12;
 
+import java.io.*;
+import javax.swing.*;
+import java.util.*;
+
 /**
  *
- * @author Juice
+ * @author Justin Espiritu
  */
 public class Lab12 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+
+        JFileChooser chosen = new JFileChooser();
+        int returnVal = chosen.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            Scanner file = new Scanner(chosen.getSelectedFile());
+            while(file.hasNext()){
+                /* did not have time to finish but wanted to make some type
+                of program that compiled.  Got stuck on my method as Dijkstra 
+                and Prim's method uses distances/weights but with the way that
+                I implemented my program I did not know how to grab the weights
+                from my edges when I am given only a vertex.
+                */
+            }
+        }
     }
-    
 }
