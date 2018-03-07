5dbd1b7249ee1358ed573c36b6c16ef1b20cf346
JuicyJuice1100 <espiritu.l.justin@gmail.com>
2018-03-06 19:40:58 -0600

    finished question 4 & 6 PL

--- Spring 2018/Programing Languages/Assignment 4/a4.js
+++ Spring 2018/Programing Languages/Assignment 4/a4.js
@@ -1,8 +1,8 @@
 var fp = require('./fp');
 var util = require('util');
 
-if ( ! exports )
-   var exports = [ ];
+if (!exports)
+    var exports = [];
 
 
 
@@ -10,18 +10,18 @@ if ( ! exports )
 // end comment below
 
 var minMax = function (lol) {
-    var min = function(l){
+    var min = function (l) {
         return fp.reduce(fp.min, fp.tl(l), fp.hd(l));
     }
-    var max = function(l){
+    var max = function (l) {
         return fp.reduce(fp.max, fp.tl(l), fp.hd(l));
     }
 
-    var minMax = function(l){
+    var minMax = function (l) {
         return fp.makeList(min(l), max(l));
     }
 
-    var reducer = function(x, y){
+    var reducer = function (x, y) {
         return fp.cons(x, y);
     }
 
@@ -39,10 +39,10 @@ var minMax = function (lol) {
 // Your solution for problem 2 must appear between this and matching
 // end comment below
 
-var deepMap = function (f,ns) {
-    if(fp.isNull(ns)){
+var deepMap = function (f, ns) {
+    if (fp.isNull(ns)) {
         return ns;
-    } else if(fp.isList(fp.hd(ns))){
+    } else if (fp.isList(fp.hd(ns))) {
         return fp.cons(deepMap(f, fp.hd(ns)), deepMap(f, fp.tl(ns)));
     } else {
         return fp.cons(f(fp.hd(ns)), deepMap(f, fp.tl(ns)));
@@ -56,16 +56,16 @@ var deepMap = function (f,ns) {
 // end comment below
 
 
-var countOccurrences = function (ns) {   
+var countOccurrences = function (ns) {
     // var counter = function(l){
     //     return fp.reduce(reducer, fp.filter(fp.curry(fp.isEq)(l), ns), 0);
     // }
 
-    var mapper = function(l){
+    var mapper = function (l) {
         return fp.append(fp.makeList(fp.hd(fp.filter(fp.curry(fp.isEq)(l), ns))), fp.makeList(fp.reduce(reducer, fp.filter(fp.curry(fp.isEq)(l), ns), 0)));
     }
 
-    var reducer = function(x, y){
+    var reducer = function (x, y) {
         return fp.add(x, 1);
     }
 
@@ -80,15 +80,15 @@ var countOccurrences = function (ns) {
 // end comment below
 
 
-var isInRect = function (t,l,b,r,point) {
-    if (fp.isLT(fp.hd(fp.tl(point)),t)) {
-	if (fp.isGT(fp.hd(fp.tl(point)),b)) {
-	    if (fp.isGT(fp.hd(point),l)) {
-		if (fp.isLT(fp.hd(point),r)) {
-		    return true;
-		}// point is to the left of r
-	    }// point is to the right of l
-	}// point is above b
+var isInRect = function (t, l, b, r, point) {
+    if (fp.isLT(fp.hd(fp.tl(point)), t)) {
+        if (fp.isGT(fp.hd(fp.tl(point)), b)) {
+            if (fp.isGT(fp.hd(point), l)) {
+                if (fp.isLT(fp.hd(point), r)) {
+                    return true;
+                }// point is to the left of r
+            }// point is to the right of l
+        }// point is above b
     }// point is below t
     return false;
 };// isInRect function
@@ -100,16 +100,26 @@ var rightEdge = 1000000;
 
 // curry5 curries a function of five parameters
 var curry5 = function (f) {
-    return 0;
+    return function (g) {
+        return function (h) {
+            return function (i) {
+                return function (j) {
+                    return function (k) {
+                        return f(g, h, i, j, k);
+                    }
+                }
+            }
+        }
+    }
 };
 
 // Use curry5 to define each of the five functions specified in the
 // assignment
-var isInFirstQuadrant = function(x) { return x; };
-var isInSecondQuadrant = function(x) { return x; };
-var isInThirdQuadrant = function(x) { return x; };
-var isInFourthQuadrant = function(x) { return x; };
-var isInUnitSquare = function(x) { return x; };
+var isInFirstQuadrant = function (x) { return curry5(isInRect)(topEdge)(0)(0)(rightEdge)(x); };
+var isInSecondQuadrant = function (x) { return curry5(isInRect)(topEdge)(leftEdge)(0)(0)(x); };
+var isInThirdQuadrant = function (x) { return curry5(isInRect)(0)(leftEdge)(bottomEdge)(0)(x); };
+var isInFourthQuadrant = function (x) { return curry5(isInRect)(0)(0)(bottomEdge)(rightEdge)(x); };
+var isInUnitSquare = function (x) { return curry5(isInRect)(1)(-1)(-1)(1)(x); };
 
 
 
@@ -119,11 +129,10 @@ var isInUnitSquare = function(x) { return x; };
 // end comment below
 
 
-var makeCompositeMFRFunc = function(mf, ff, rf, acc)  {
-
+var makeCompositeMFRFunc = function (mf, ff, rf, acc) {
     return 0;
 };
-    
+
 
 ////////// End of code for problem 5 ////////////////////
 
@@ -132,7 +141,19 @@ var makeCompositeMFRFunc = function(mf, ff, rf, acc)  {
 
 
 var howManyWithLargerGTSmaller = function (list) {
-    return 0;
+    var mapper = function(x){
+        return fp.reduce(fp.isLT, fp.tl(x), fp.hd(x));
+    }
+
+    var reducer = function(x, y){
+        return fp.add(x, 1);
+    }
+
+    return fp.reduce(
+        reducer,
+        fp.filter(fp.curry(fp.isEq)(true), fp.map(mapper, minMax(list))),
+        0
+    )
 };
 
 
@@ -144,63 +165,63 @@ var howManyWithLargerGTSmaller = function (list) {
 ////////// submission is evaluated
 
 console.log("Testing Problem 1");
-console.log(minMax( [[1,2,3],[5,4],[6],[7,10,9,9,10]] ));
+console.log(minMax([[1, 2, 3], [5, 4], [6], [7, 10, 9, 9, 10]]));
 
 console.log("Testing Problem 2");
-console.log(deepMap(function(x) { return x + 1; }, [1, 2, [3, 4, 5], 6]));
-console.log(deepMap(function(x) { return 2 * x; }, [[1,2,3],[5,4],[6],[7,10,[9,9],10]] ));
+console.log(deepMap(function (x) { return x + 1; }, [1, 2, [3, 4, 5], 6]));
+console.log(deepMap(function (x) { return 2 * x; }, [[1, 2, 3], [5, 4], [6], [7, 10, [9, 9], 10]]));
 
 console.log("Testing Problem 3");
-console.log(countOccurrences( [5] ));
-console.log(countOccurrences( [1,2,3] ));
-console.log(countOccurrences( [1,2,3,2,3,1,4,5,1] ));
+console.log(countOccurrences([5]));
+console.log(countOccurrences([1, 2, 3]));
+console.log(countOccurrences([1, 2, 3, 2, 3, 1, 4, 5, 1]));
 
 console.log("Testing Problem 4");
 
-console.log(isInFirstQuadrant([1,1]));
-console.log(isInFirstQuadrant([-1,1]));
-console.log(isInFirstQuadrant([-1,-1]));
-console.log(isInFirstQuadrant([1,-1]));
-console.log(isInSecondQuadrant([1,1]));
-console.log(isInSecondQuadrant([-1,1]));
-console.log(isInSecondQuadrant([-1,-1]));
-console.log(isInSecondQuadrant([1,-1]));
-console.log(isInThirdQuadrant([1,1]));
-console.log(isInThirdQuadrant([-1,1]));
-console.log(isInThirdQuadrant([-1,-1]));
-console.log(isInThirdQuadrant([1,-1]));
-console.log(isInFourthQuadrant([1,1]));
-console.log(isInFourthQuadrant([-1,1]));
-console.log(isInFourthQuadrant([-1,-1]));
-console.log(isInFourthQuadrant([1,-1]));
-console.log(isInUnitSquare([0,0]));
+console.log(isInFirstQuadrant([1, 1]));
+console.log(isInFirstQuadrant([-1, 1]));
+console.log(isInFirstQuadrant([-1, -1]));
+console.log(isInFirstQuadrant([1, -1]));
+console.log(isInSecondQuadrant([1, 1]));
+console.log(isInSecondQuadrant([-1, 1]));
+console.log(isInSecondQuadrant([-1, -1]));
+console.log(isInSecondQuadrant([1, -1]));
+console.log(isInThirdQuadrant([1, 1]));
+console.log(isInThirdQuadrant([-1, 1]));
+console.log(isInThirdQuadrant([-1, -1]));
+console.log(isInThirdQuadrant([1, -1]));
+console.log(isInFourthQuadrant([1, 1]));
+console.log(isInFourthQuadrant([-1, 1]));
+console.log(isInFourthQuadrant([-1, -1]));
+console.log(isInFourthQuadrant([1, -1]));
+console.log(isInUnitSquare([0, 0]));
 
 
 console.log("Testing Problem 5");
 // Uncomment the lines below once you are ready to test makeCompositeMFRFunc
-// console.log(
-//     makeCompositeMFRFunc(
-// 	function(x) {return x + 1;},
-// 	function(x) {return x > 0;},
-// 	fp.add,
-// 	0)
-//     ([0,0,-1,-1,4,3]) 
-// 	   );
-// 
-// 
-// console.log(
-//     makeCompositeMFRFunc(
-// 	function(x) {return x + 3;},
-// 	function(x) {return fp.isZero(x % 2);},
-// 	function(a,n) { return fp.cons(n, a); },
-// 	[])
-//     ([11,15,12,4,3]) 
-// 	   );
+console.log(
+    makeCompositeMFRFunc(
+        function (x) { return x + 1; },
+        function (x) { return x > 0; },
+        fp.add,
+        0)
+        ([0, 0, -1, -1, 4, 3])
+);
+
+
+console.log(
+    makeCompositeMFRFunc(
+        function (x) { return x + 3; },
+        function (x) { return fp.isZero(x % 2); },
+        function (a, n) { return fp.cons(n, a); },
+        [])
+        ([11, 15, 12, 4, 3])
+);
 
 
 console.log("Testing Problem 6");
-console.log( howManyWithLargerGTSmaller ( [] ));
-console.log( howManyWithLargerGTSmaller ( [ [1,2,3] ] ));
-console.log( howManyWithLargerGTSmaller ( [ [1,1],[2] ] ));
-console.log( howManyWithLargerGTSmaller ( [ [1,2,3], [2,2],[1,-1] ] ));
+console.log(howManyWithLargerGTSmaller([]));
+console.log(howManyWithLargerGTSmaller([[1, 2, 3]]));
+console.log(howManyWithLargerGTSmaller([[1, 1], [2]]));
+console.log(howManyWithLargerGTSmaller([[1, 2, 3], [2, 2], [1, -1]]));
 
