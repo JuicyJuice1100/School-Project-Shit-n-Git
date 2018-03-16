c3cc86ccef7007f3034b448b0219d67317397bad
JuicyJuice1100 <espiritu.l.justin@gmail.com>
2018-03-15 20:03:21 -0500

    Finished initial design of mobile app

---
+++ Spring 2018/Mobile Dev/FinalProject/app/src/main/java/com/example/juice/nerdup/MainPortraitLayout.java
@@ -0,0 +1,13 @@
+package com.example.juice.nerdup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPortraitLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_portrait_layout);
+    }
+}
