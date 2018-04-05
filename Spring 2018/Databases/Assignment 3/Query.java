e8921308362ef955b13fe4dae31830e5a9f5939f
JuicyJuice1100 <espiritu.l.justin@gmail.com>
2018-04-04 22:31:02 -0500

    did some pl assignment 6 and finish database assignment 3

--- Spring 2018/Databases/Assignment 3/Query.java
+++ Spring 2018/Databases/Assignment 3/Query.java
@@ -5,6 +5,7 @@ public class Query{
     private String query, parameters;
     private int num;
     private Scanner scan;
+    private static Properties config;
 
     public Query(){
         scan = new Scanner(System.in);
@@ -12,27 +13,95 @@ public class Query{
         num = scan.next();
         parameters = scan.next();
     }
-
+        
+    private static void loadProperties() {
+        try {
+            FileInputStream file = new FileInputStream("config.properties");
+            config = new Properties();
+            config.load(file);
+            file.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred while trying to load config.properties.");
+        }
+    }   
+    
     public static void main(String [] args){
         QueryCall queryCall;
         Query query = new Query();
-        Connect connect = new Connnect();
+        String queryString;
+        PreparedStatement pstmt;
+        ResultSet results;
+        
+        //Load properties file
+        loadProperties();
+        String db = config.getProperty("database");
+        String user = config.getProperty("username");
+        String pw = config.getProperty("password");
+        String driver = config.getProperty("driver");
+        String url = config.getProperty("url") + db;
+    
+          try{
+              Class.forName(driver);  // register the driver
+              System.out.println("Attempting connection");
+              Connection conn = DriverManager.getConnection(url,user,pw);
+              System.out.println("Connection successfully made");
+          }
+          catch (SQLException e) 
+          {System.out.println("SQL Error " + e.getMessage());}
+          catch (Exception e) 
+          {System.out.println("General Error " + e.getMessage());}
 
         if(query.query.equalsIgnoreCase("Query")){
             switch(query.num){
                 case 1:
-                    //insert query here
+                    queryString = "SELECT Customer.cname FROM Customer JOIN Loan ON Loan.cname = Customer.cname JOIN Branch ON Branch.bname = Loan.bname WHERE assets > ? ORDER BY Customer.cname ASC";
+                    pstmt=conn.prepareStatement(query);
+                    pstmt.clearParameters(); 
+            
+                    pstmt.setString(1, query.parameters);
+                    results = pstmt.executeQuery ();
+                    System.out.println (" Results \n");
+                    while ( results.next ()){
+                        String customer = results.getString ("cname");
+                        System.out.println(customer);
+                    }
                     break;
                 case 2:
-                    //insert query here
+                    queryString = "SELECT Customer.cname FROM Customer JOIN Loan On Loan.cname = Customer.cname GROUP BY cname HAVING SUM(amount) > ?";
+                    pstmt=conn.prepareStatement(query);
+                    pstmt.clearParameters(); 
+            
+                    pstmt.setString(1, query.parameters);
+                    results = pstmt.executeQuery ();
+                    System.out.println (" Results \n");
+                    while ( results.next ()){
+                        String customer = results.getString ("cname");
+                        System.out.println(customer);
+                    }
                     break;
                 case 3:
-                    //insert query here
+                    queryString = "SELECT COALESCE(daccnum, \"n/a\"), COALESCE(balance, 0), COALESCE(Deposit.bname, \"n/a\"), COALESCE(laccnum, \"n/a\"), COALESCE(amount, 0), COALESCE(Loan.bname, \"n/a\")" + 
+                    " FROM Deposit RIGHT JOIN Customer ON Customer.cname = Deposit.cname LEFT JOIN Loan On Loan.cname = Customer.cname WHERE Customer.cname = ?";
+                    pstmt=conn.prepareStatement(query);
+                    pstmt.clearParameters(); 
+            
+                    pstmt.setString(1, query.parameters);
+                    results = pstmt.executeQuery ();
+                    System.out.println (" Results \n");
+                    while ( results.next ()){
+                        String daccnum = results.getString ("daccnum");
+                        String balance = results.getString("balance");
+                        String dbname = results.getString("Deposit.bname");
+                        String laccnum = results.getString("laccnum");
+                        String amount = results.getString("amount");
+                        String lbname = results.getString("loan.bname");
+                        System.out.println(daccnum + "," + balance + "," + dbname  + "," + laccnum + "," + amount + "," + lbname);
+                    }
                     break;
                 default:
                     System.out.println("invalid query call");
             }
-            connect.conn.close();
+            conn.close();
         } else {
             System.out.println("invalid query call");
         }
