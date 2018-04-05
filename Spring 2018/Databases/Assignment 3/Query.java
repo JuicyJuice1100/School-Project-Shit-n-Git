8a564387c44718ed18ef25261f52a7b33c723897
JuicyJuice1100 <espiritu.l.justin@gmail.com>
2018-04-05 12:50:48 -0500

    updated assignment 3 databases

--- Spring 2018/Databases/Assignment 3/Query.java
+++ Spring 2018/Databases/Assignment 3/Query.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.sql.*;
+import java.io.FileInputStream;
+import java.util.Properties;
 
 public class Query{
     private String query, parameters;
@@ -9,8 +11,8 @@ public class Query{
 
     public Query(){
         scan = new Scanner(System.in);
-        query = scan.nextInt();
-        num = scan.next();
+        query = scan.next();
+        num = scan.nextInt();
         parameters = scan.next();
     }
         
@@ -26,11 +28,11 @@ public class Query{
     }   
     
     public static void main(String [] args){
-        QueryCall queryCall;
         Query query = new Query();
         String queryString;
         PreparedStatement pstmt;
         ResultSet results;
+        Connection conn = null;
         
         //Load properties file
         loadProperties();
@@ -43,7 +45,7 @@ public class Query{
           try{
               Class.forName(driver);  // register the driver
               System.out.println("Attempting connection");
-              Connection conn = DriverManager.getConnection(url,user,pw);
+              conn = DriverManager.getConnection(url,user,pw);
               System.out.println("Connection successfully made");
           }
           catch (SQLException e) 
@@ -54,54 +56,74 @@ public class Query{
         if(query.query.equalsIgnoreCase("Query")){
             switch(query.num){
                 case 1:
-                    queryString = "SELECT Customer.cname FROM Customer JOIN Loan ON Loan.cname = Customer.cname JOIN Branch ON Branch.bname = Loan.bname WHERE assets > ? ORDER BY Customer.cname ASC";
-                    pstmt=conn.prepareStatement(query);
-                    pstmt.clearParameters(); 
-            
-                    pstmt.setString(1, query.parameters);
-                    results = pstmt.executeQuery ();
-                    System.out.println (" Results \n");
-                    while ( results.next ()){
-                        String customer = results.getString ("cname");
-                        System.out.println(customer);
+                    try{
+                        queryString = "SELECT Customer.cname FROM Customer JOIN Loan ON Loan.cname = Customer.cname JOIN Branch ON Branch.bname = Loan.bname WHERE assets > ? ORDER BY Customer.cname ASC";
+                        pstmt=conn.prepareStatement(queryString);
+                        pstmt.clearParameters(); 
+                
+                        pstmt.setString(1, query.parameters);
+                        results = pstmt.executeQuery ();
+                        System.out.println (" Results \n");
+                        while ( results.next ()){
+                            String customer = results.getString ("cname");
+                            System.out.println(customer);
+                        }
+                        conn.close();
                     }
+                    catch (SQLException e) 
+                    {System.out.println("SQL Error " + e.getMessage());}
+                    catch (Exception e) 
+                    {System.out.println("General Error " + e.getMessage());}
                     break;
                 case 2:
-                    queryString = "SELECT Customer.cname FROM Customer JOIN Loan On Loan.cname = Customer.cname GROUP BY cname HAVING SUM(amount) > ?";
-                    pstmt=conn.prepareStatement(query);
-                    pstmt.clearParameters(); 
-            
-                    pstmt.setString(1, query.parameters);
-                    results = pstmt.executeQuery ();
-                    System.out.println (" Results \n");
-                    while ( results.next ()){
-                        String customer = results.getString ("cname");
-                        System.out.println(customer);
+                    try{
+                       queryString = "SELECT Customer.cname FROM Customer JOIN Loan On Loan.cname = Customer.cname GROUP BY cname HAVING SUM(amount) > ?";
+                       pstmt=conn.prepareStatement(queryString);
+                       pstmt.clearParameters(); 
+                
+                       pstmt.setString(1, query.parameters);
+                       results = pstmt.executeQuery ();
+                       System.out.println (" Results \n");
+                       while ( results.next ()){
+                           String customer = results.getString ("cname");
+                           System.out.println(customer);
+                       } 
+                       conn.close();
                     }
+                    catch (SQLException e) 
+                    {System.out.println("SQL Error " + e.getMessage());}
+                    catch (Exception e) 
+                    {System.out.println("General Error " + e.getMessage());}
                     break;
                 case 3:
-                    queryString = "SELECT COALESCE(daccnum, \"n/a\"), COALESCE(balance, 0), COALESCE(Deposit.bname, \"n/a\"), COALESCE(laccnum, \"n/a\"), COALESCE(amount, 0), COALESCE(Loan.bname, \"n/a\")" + 
-                    " FROM Deposit RIGHT JOIN Customer ON Customer.cname = Deposit.cname LEFT JOIN Loan On Loan.cname = Customer.cname WHERE Customer.cname = ?";
-                    pstmt=conn.prepareStatement(query);
-                    pstmt.clearParameters(); 
-            
-                    pstmt.setString(1, query.parameters);
-                    results = pstmt.executeQuery ();
-                    System.out.println (" Results \n");
-                    while ( results.next ()){
-                        String daccnum = results.getString ("daccnum");
-                        String balance = results.getString("balance");
-                        String dbname = results.getString("Deposit.bname");
-                        String laccnum = results.getString("laccnum");
-                        String amount = results.getString("amount");
-                        String lbname = results.getString("loan.bname");
-                        System.out.println(daccnum + "," + balance + "," + dbname  + "," + laccnum + "," + amount + "," + lbname);
+                    try{
+                        queryString = "SELECT COALESCE(daccnum, \"n/a\"), COALESCE(balance, 0), COALESCE(Deposit.bname, \"n/a\"), COALESCE(laccnum, \"n/a\"), COALESCE(amount, 0), COALESCE(Loan.bname, \"n/a\")" + 
+                        " FROM Deposit RIGHT JOIN Customer ON Customer.cname = Deposit.cname LEFT JOIN Loan On Loan.cname = Customer.cname WHERE Customer.cname = ?";
+                        pstmt=conn.prepareStatement(queryString);
+                        pstmt.clearParameters(); 
+                
+                        pstmt.setString(1, query.parameters);
+                        results = pstmt.executeQuery ();
+                        System.out.println (" Results \n");
+                        while ( results.next ()){
+                            String daccnum = results.getString ("COALESCE(daccnum, \"n/a\")");
+                            String balance = results.getString("COALESCE(balance, 0)");
+                            String dbname = results.getString("COALESCE(Deposit.bname, \"n/a\")");
+                            String laccnum = results.getString("COALESCE(laccnum, \"n/a\")");
+                            String amount = results.getString("COALESCE(amount, 0)");
+                            String lbname = results.getString("COALESCE(loan.bname, \"n/a\")");
+                            System.out.println(daccnum + "," + balance + "," + dbname  + "," + laccnum + "," + amount + "," + lbname);
+                        }
+                        conn.close();
                     }
+                    catch (SQLException e) 
+                    {System.out.println("SQL Error " + e.getMessage());}
+                    catch (Exception e) 
+                    {System.out.println("General Error " + e.getMessage());}
                     break;
                 default:
                     System.out.println("invalid query call");
             }
-            conn.close();
         } else {
             System.out.println("invalid query call");
         }
